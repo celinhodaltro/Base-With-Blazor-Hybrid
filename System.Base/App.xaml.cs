877638d096a4a5bbbce9826ec85f90a064254283
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-06-18 13:47:26 -0300

    InitialCommit

---
+++ System.Base/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Base
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
